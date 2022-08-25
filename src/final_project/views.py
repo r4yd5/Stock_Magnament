60b8978ad754492c9dff6bd44f8de165000edd86
r4yd <juanmsanchez0640@gmail.com>
2022-08-25 13:31:13 -0300

    version: terminar about

--- src/final_project/views.py
+++ src/final_project/views.py
@@ -1,1 +1,5 @@
+from django.shortcuts import render,redirect
 
+
+def sobre_mi(request):
+    return 
--- src/messages/models.py
+++ src/messages/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class Messages(models.Model):
     mensaje = models.CharField(max_length=10000)
     usuario = models.CharField(max_length=10000)
+
+    def __str__(self):
+        return f'Mensaje de: {self.usuario}'
+    
+    
